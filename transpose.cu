ab3bd6a7de617d4c4ce6bbd57d91e659bdddb0a8
Michael Castillo <mvcastillo@wave8-login1.wave.scu.edu>
2024-08-16 18:45:46 -0700

    Expounding on fun (soon to be renamed).

--- transpose.cu
+++ transpose.cu
@@ -4,7 +4,12 @@
  * We want to divide the work of each row of the CSC into its own designated Block
  * Correspondingly, each thread within each block will deal with it's own non-zero value from the CSR
  * 
- * We begin by 
+ * We begin by establishing j value (unique non-zero identifier),
+ *      then doing a lookup into said value and index of CSR. 
+ * We then set the transposed position of value and index (using ptr row value) to said value and index.
+ * 
+ * A Final increment of the ptr value in question is necessary, to point to the next spot in the chain,
+ *      for the next value index pair to be slotted in. Changes to ptr non-binding, b/c we won't cpy it back.
  */
 
 __global__ void func(float * CSRval, int * CSRind, float * CSCval, int * CSCind, int * CSCptr) {
@@ -12,7 +17,6 @@ __global__ void func(float * CSRval, int * CSRind, float * CSCval, int * CSCind,
     int high = CSCptr[blockIdx.x + 1];
     int low = CSCptr[blockIdx.x];
     int guard = high - low;
-    
 
     /*
      * Each thread is the product of the block dimensions. 
@@ -22,16 +26,19 @@ __global__ void func(float * CSRval, int * CSRind, float * CSCval, int * CSCind,
      */
     if (threadIdx.x < guard){
         
-        int j = threadIdx.x + low;                    //Retrieving assigned non-zero value, offset for row
-        float v = CSRval[j];
-        int c = CSRind[j];
-        CSCval[CSCptr[c]] = low;
-        CSCind[CSCptr[c]] = v;
+        int j = threadIdx.x + low;                      //Retrieving assigned non-zero value, offset for row
+        float v = CSRval[j];                            //Grab future value
+        int c = CSRind[j];                              //Grab future row
+        /*
+         * Set future value (position determined by current spot open in row) properly
+         * Set future index similarly, with Block ID (row num in CSR) accordingly
+         * Only works b/c we increment the open position within each row
+         */
+        CSCval[CSCptr[c]] = v;
+        CSCind[CSCptr[c]] = low;
         ++CSCptr[c];
 
     } else return;
-
-
 }
 
 
